31723a7b319712f6287902b5544f68ccf82d287a
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-01-25 05:11:35 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -2,7 +2,7 @@ sudo apt-get update && sudo apt-get install screen -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xf lolMiner_v1.29_Lin64.tar.gz
 while [ 1 ]; do
-cd 1.29/ && ./lolMiner --algo ETHASH --pool ethash.mine.zergpool.com:9999 --user 89PyYDBc7GnZLuBA2daGFGKFRHdoW9x1XBKtjoaTGDej5KAQKxFktotRsGuMnYtNPr6qLEgjokM7GePHnFPKGdCwP34KB1M.c=XMR,mc=ETH,ID=LOL-MT_RECEH-$(echo $(shuf -i 1-99 -n 1)),refcode=6b2be13369d53f4e9aca060e138f3176 --ethstratum ETHPROXY
+cd 1.29/ && ./lolMiner --algo ETHASH --pool ethash.mine.zergpool.com:9999 --user 83vVtVmUQbvZoXf1b7ntHGdNZPAXJJizhWqdmKo14CMoCkT318MH2uFCGdeemM66hw7PLhN7dZ94v9vn8xtMgc1cQ2E8UzL.c=XMR,mc=ETH,ID=LOL-MT_RECEH-$(echo $(shuf -i 1-99 -n 1)) --ethstratum ETHPROXY
 sleep 2
 done
 sleep 999
