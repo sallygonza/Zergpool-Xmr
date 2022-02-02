0c11024dc7705eef4ceae8e3ded97b4f517d89b6
sallygonza <90584824+sallygonza@users.noreply.github.com>
2022-02-02 09:30:17 +0700

    Update sol.sh

--- sol.sh
+++ sol.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 WORKER=$(echo $(shuf -i 10-40 -n 1))
 sudo apt-get update && sudo apt-get install screen -y
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar -xf lolMiner_v1.29_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.42/lolMiner_v1.42_Lin64.tar.gz
+tar -xf lolMiner_v1.42_Lin64.tar.gz
 while [ 1 ]; do
-cd 1.29/ && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SOL:2gQKjdGD6BFMk7hnA6xzZvg9EbnPzUErHdyi39uqfLvf.$WORKER --ethstratum ETHPROXY
+cd 1.42/ && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SOL:2gQKjdGD6BFMk7hnA6xzZvg9EbnPzUErHdyi39uqfLvf.$WORKER --ethstratum ETHPROXY
 sleep 2
 done
 sleep 999
